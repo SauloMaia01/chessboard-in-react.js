329a56650249217ebc261c7f03cf27f08bd1e9d5
SauloMaia01 <saulormaia@hotmail.com>
2022-06-28 16:54:56 -0300

    first commit

---
+++ components/Tabuleiro.jsx
@@ -0,0 +1,16 @@
+import Linha from "./Linha"
+
+export default function Tabuleiro() {
+    return (
+        <div>
+            <linha />
+            <linha preta />
+            <linha />
+            <linha preta />
+            <linha />
+            <linha preta />
+            <linha />
+            <linha preta />
+        </div>
+    )
+}
